c000bcd227931482d9db1c4f33db239f18215e39
Smart Comuna Dev <estud@estudiovls.com>
2026-03-04 14:25:53 -0300

    Fix blank screen on GitHub pages with HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import FiscalizacionForm from './modules/fiscalizacion/components/FiscalizacionForm.jsx';
 
 function Home() {
